ac3d988884812e6565d689891a1999fa6cd4be28
Carlo De Pieri <depieri.carlo@gmail.com>
2022-03-18 18:14:25 +0100

    add support for xvfb virtual X server

--- tasks.py
+++ tasks.py
@@ -1,6 +1,18 @@
 from invoke import task
+from xvfbwrapper import Xvfb
 
 
 @task
 def run(c):
     c.run("poetry run uvicorn rss_parser.api:app")
+
+
+@task
+def run_xvfb(c):
+    """Launch this if no real X display is available. Needs xvfb installed."""
+    display = Xvfb()
+    display.start()
+    try:
+        c.run("poetry run uvicorn --host 0.0.0.0 rss_parser.api:app")
+    finally:
+        display.stop()
